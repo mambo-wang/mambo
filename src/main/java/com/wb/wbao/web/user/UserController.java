0d82d642a3be0dfcd4b5e7a9d6b5ffa74b46e40c
mambo1991@163.com <wangbao1991>
2017-09-22 09:03:32 +0800

    时间：9月22日 改动：1、shiro开发，实战，加@requireRoles和@requirePermissions注解实现授权。

--- src/main/java/com/wb/wbao/web/user/UserController.java
+++ src/main/java/com/wb/wbao/web/user/UserController.java
@@ -5,6 +5,8 @@ import com.wb.wbao.server.user.User;
 import com.wb.wbao.server.user.UserMgr;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -37,6 +39,8 @@ public class UserController {
 
     @ApiOperation(value = "创建用户",notes = "根据用户名、密码、入学年份、用户姓名创建用户")
     @ApiImplicitParam(name = "user", value = "用户", required = true, dataType = "User")
+    @RequiresRoles("admin")
+    @RequiresPermissions("user:add")
     @PostMapping
     public List<UserDTO> createUser(@RequestBody UserDTO userDTO){
         userMgr.createUser(userDTO);
@@ -46,6 +50,7 @@ public class UserController {
 
     @ApiOperation(value = "查询用户", notes = "查询所有用户")
     @GetMapping
+    @RequiresPermissions("user:view")
     public List<UserDTO> queryAllUsers() {
         return userMgr.queryAll();
     }
