9a3b339644c114478ac41dfabe9afab56d645d3c
mambo-wang <mambo1991@163.com>
2018-07-01 15:11:14 +0800

    时间：7月1日：1、使用ThreadMXBean进行死锁检测

--- src/test/java/com/wb/wbao/multithread/lock/DeadLockSample.java
+++ src/test/java/com/wb/wbao/multithread/lock/DeadLockSample.java
@@ -1,5 +1,12 @@
 package com.wb.wbao.multithread.lock;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class DeadLockSample extends Thread {
     private String first;
     private String second;
@@ -22,6 +29,23 @@ public class DeadLockSample extends Thread {
         }
     }
     public static void main(String[] args) throws InterruptedException {
+
+        ThreadMXBean mbean = ManagementFactory.getThreadMXBean();
+        Runnable dlCheck = () -> {
+            long[] threadIds = mbean.findDeadlockedThreads();
+            if (threadIds != null) {
+                ThreadInfo[] threadInfos = mbean.getThreadInfo(threadIds);
+                System.out.println("Detected deadlock threads:");
+                for (ThreadInfo threadInfo : threadInfos) {
+                    System.out.println(threadInfo.getThreadName());
+                }
+            }
+        };
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        // 稍等 5 秒，然后每 10 秒进行一次死锁扫描
+        scheduler.scheduleAtFixedRate(dlCheck, 5L, 10L, TimeUnit.SECONDS);
+
+
         String lockA = "lockA";
         String lockB = "lockB";
         DeadLockSample t1 = new DeadLockSample("Thread1", lockA, lockB);
@@ -34,8 +58,12 @@ public class DeadLockSample extends Thread {
         //输出：
         //Thread1 obtained: lockA
         //Thread2 obtained: lockB
+        //Detected deadlock threads:
+        //Thread2
+        //Thread1
 
-        //先用任务管理器查看本进程的pid，再使用jstack pid来分析查找检测死锁
+        //死锁检测方式1：先用任务管理器查看本进程的pid，再使用jstack pid来分析查找检测死锁
+        //死锁检测方式2：Java 提供的标准管理 API，ThreadMXBean
 
     }
 }
